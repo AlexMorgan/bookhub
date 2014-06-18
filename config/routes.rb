f865ec4bd3dbbe1a14e1fa59884edf102e9249f0
AlexMorgan <alexmorgan.am@gmail.com>
2014-06-18 18:41:38 -0400

    Finished user registration and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  devise_for :users
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root to: "home#index"
+  root "pages#home"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
