d7d760fcee3789a25918773924d5d7ec88ad6684
AlexMorgan <alexmorgan.am@gmail.com>
2014-06-19 09:57:45 -0400

    Changed the navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :tiny => "25x25>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  ## If we wanted to validate that all users had an avatar
+  # validates :avatar, :attachment_presence => true
   validates :username, presence: true, uniqueness: true
 end
