e8bd0790d0bf2ad07a172b374df7db71a9ffd56a
AlexMorgan <alexmorgan.am@gmail.com>
2014-06-19 18:29:57 -0400

    Added multiple users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
+      marker.infowindow user.username
     end
   end
 
