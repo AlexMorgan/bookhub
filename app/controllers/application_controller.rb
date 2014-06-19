268b8f396bc4775dce77794993e256a6986e51d4
AlexMorgan <alexmorgan.am@gmail.com>
2014-06-18 23:29:19 -0400

    Added feature to allow username updates without having to enter password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,35 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+# class ApplicationController < ActionController::Base
+#   # Prevent CSRF attacks by raising an exception.
+#   # For APIs, you may want to use :null_session instead.
+#   protect_from_forgery with: :exception
 
+#   before_action :authenticate_user!, except: :home
+#   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+#   protected
+
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.for(:sign_up) << :username
+#     devise_parameter_sanitizer.for(:account_update) << :username
+#   end
+
+
+# end
+
+
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_user!, except: :home
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:account_update) << :username
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:username, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:username, :email, :password, :password_confirmation)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root "pages#home"
