ce787ac091a6149c01f9aac4d0e6e70f0be33753
AlexMorgan <alexmorgan.am@gmail.com>
2014-06-19 16:14:28 -0400

    got rid of required images

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>",small: "50x50>", :tiny => "25x25>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>",small: "50x50>", :tiny => "25x25>" }, :default_url => "/assets/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   ## If we wanted to validate that all users had an avatar
-  validates :avatar, :attachment_presence => true
+  # validates :avatar, :attachment_presence => true
   validates :username, presence: true, uniqueness: true
 end
