ce54a2539226400b95a11ffff9c2e5d63929a885
AlexMorgan <alexmorgan.am@gmail.com>
2014-06-20 17:55:04 -0400

    typo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,7 +14,7 @@ class RegistrationsController < Devise::RegistrationsController
     if successfully_updated
       set_flash_message :notice, :updated
       # Sign in the user bypassing validation in case their password changed
-      sign_in @user, :bypass => true
+      sign_in @user, bypass: true
       redirect_to after_update_path_for(@user)
     else
       render "edit"
