6822e06d16d59d6af88096cdc21f76e81cf013e1
AlexMorgan <alexmorgan.am@gmail.com>
2014-06-22 21:16:45 -0400

    Small edits and finetuning of user dashboard

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ActionMailer::Base
     @seller = seller
     @buyer = buyer
     @book = book
-    binding.pry
 
     @url  = 'http://www.launchacademy.com'
-    mail(to: @buyer.email, subject: 'One of your items have been purchased!')
+    mail(to: @seller.email, subject: "Purchase Inquiry: #{@book.title} has been sold!")
   end
 end
