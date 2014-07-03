f9b633664376b2ddd18e0e0dcc160e741fde288f
AlexMorgan <alexmorgan.am@gmail.com>
2014-07-03 16:32:00 -0400

    added peer offers to user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,11 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    sql = "SELECT count(offers.id) FROM offers JOIN books
+          ON offers.book_id = books.id JOIN users ON users.id = books.user_id
+          WHERE users.id = #{@user.id} AND books.sold = false;"
+
+    @offer_count = ActiveRecord::Base.connection.execute(sql)
+    @offer_count = @offer_count.first["count"]
   end
 end
