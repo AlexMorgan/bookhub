25a4225add1b051e83e22128555d824702dd4e43
AlexMorgan <alexmorgan.am@gmail.com>
2014-07-15 14:42:47 -0400

    Working on updatesearch sidekiq worker

--- app/workers/update_search.rb
+++ app/workers/update_search.rb
@@ -1,27 +1,26 @@
 class UpdateSearch
   include Sidekiq::Worker
-  include Sidetiq::Schedulable
   sidekiq_options retry: false
 
-  recurrence { hourly.minute_of_hour(0, 10, 20, 30, 40, 50) }
-
-  def perform
+  def perform(book_id)
+    book = Book.find(book_id)
     needs = Need.all
     matches = []
-    needs.each do |need|
-      if need.notified == false
-        if need.isbn.present?
-          matches = Book.where(isbn: need.isbn)
-        else
-          matches = Book.where(isbn13: need.isbn13)
-        end
-      end
 
-      if matches.length != 0 &&  need.notified == false
+    needs.each do |need|
+      if need.isbn.present? && book.isbn.present?
+        if need.isbn == book.isbn
+          matches << book
           UserMailer.wishlist_match(matches, need).deliver
           need.update!(notified: true)
+        end
+      else
+        if need.isbn13 == book.isbn13
+          matches << book
+          UserMailer.wishlist_match(matches, need).deliver
+          need.update!(notified: true)
+        end
       end
     end
-
   end
 end
