86077ceebb6985b8577e49448a32af409488d1f5
AlexMorgan <alexmorgan.am@gmail.com>
2014-07-14 23:59:19 -0400

    Touched up Dashboard UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    @recent_activities = Book.order(created_at: :desc).limit(3)
     sql = "SELECT count(offers.id) FROM offers JOIN books
           ON offers.book_id = books.id JOIN users ON users.id = books.user_id
           WHERE users.id = #{@user.id} AND books.sold = false;"
