6fb05dcb7b67425e0e1e40dbb03f5b8199524d2f
AlexMorgan <alexmorgan.am@gmail.com>
2014-07-15 16:57:39 -0400

    set hostname for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
+
   ActionMailer::Base.smtp_settings = {
       :port =>           '587',
       :address =>        'smtp.mandrillapp.com',
