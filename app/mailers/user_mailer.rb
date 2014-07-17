34408a531eeacd74421d233662947a0396de1a98
AlexMorgan <alexmorgan.am@gmail.com>
2014-07-17 15:21:55 -0400

    Fixed match emails to contain a link for all matches in the email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "dukehub@gmail.com"
+  default from: "boookhub@gmail.com"
 
   def purchase_email(buyer, seller, book)
     @seller = seller
     @buyer = buyer
     @book = book
 
-    @url  = 'http://www.launchacademy.com'
     mail(to: @seller.email, subject: "Purchase Inquiry: #{@book.title} has been sold!")
   end
 
