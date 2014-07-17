95bd911b7dd0d1267b53fb9ee52c2f01f377629d
AlexMorgan <alexmorgan.am@gmail.com>
2014-07-17 17:23:52 -0400

    Changed authenticating users to not allow any actions unless signed in

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :search]
+  before_action :authenticate_user!
   before_action :correct_user, only: [:edit, :destroy]
   def index
     @books = Book.all.order(created_at: :desc)
